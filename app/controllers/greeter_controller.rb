01e2cc9ed785a8bf89424974267ee5bb1fb64708
Diego de Souza <diegotrus7@gmail.com>
2015-09-29 23:14:55 -0400

    Ready to deploy

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,5 +1,10 @@
 class GreeterController < ApplicationController
   def hello #action(methods)
+    random_names = ["Alex", "Joe", "Michael"]
+    @name = random_names.sample
+    @time = Time.now
+    @times_displayed ||= 0
+    @times_displayed += 1
   end
 
   def goodbye
